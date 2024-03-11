3a725fcb302ee339029e4db8b6144dcebf976ecf
josuviteri <josu.viteri@opendeusto.es>
2024-03-11 20:05:00 +0100

    hook implementation

---
+++ HorizonTravels/src/test/MainTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MainTest {
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+
+
+	@Test
+	public void test() {
+		assertTrue("Implementation of hooks" , true);
+	}
+
+}
